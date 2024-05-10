859ffc385280de4d2e6b1e063ee1f4fdd322db3a
Felipe Vera <felipevera10@gmail.com>
2024-05-10 15:06:59 -0400

    flex in cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class=" bg-white rounded cursor-pointer md:hover:-translate-y-2 md:transition-all">
-      <div class="border flex justify-center items-center rounded w-32 h-30 md:w-36 md:h-34 px-2 text-center ">
+   <div class=" bg-white flex justify-center items-center rounded cursor-pointer md:hover:-translate-y-2 md:transition-all">
+      <div class="rounded w-32 h-30 md:w-36 md:h-34 px-2 text-center ">
          <img v-if="bankData.logo" :src="bankData.logo" alt="" class="rounded">
          <h1 v-else class="">{{ bankData.name }}</h1>
       </div>
